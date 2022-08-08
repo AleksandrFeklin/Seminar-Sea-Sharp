605c0503a04bf9299ecb8862b0ae413803324deb
Aleksandr <i@afeklin.ru>
2022-08-08 19:29:31 +0300

    Программа проверки корня числа +-

---
+++ square+-/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Программа проверки является ли первое число квадратом второго");
+
+Console.WriteLine("Введите первое число");
+
+string numberString = Console.ReadLine();
+
+int numberA = int.Parse(numberString);
+
+Console.WriteLine("Введите второе число");
+
+string numberStringA = Console.ReadLine();
+
+int numberB = int.Parse(numberStringA);
+
+int sgrt = numberA * numberA;
+
+if(numberB == sgrt)
+{
+    Console.WriteLine("Да");
+}
+
+else
+{
+    Console.WriteLine("нет");
+}
