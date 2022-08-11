962a285b85e99a6141a18b4c251bedd69f8fcda3
Aleksandr <i@afeklin.ru>
2022-08-11 16:00:29 +0300

    Изменения

---
+++ 3-ч значное число и удаление второй цифры/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа вывода случайного трехзначного числа и удаления второй цифры этого числа
+
+Random random = new Random();
+
+int randomNumber = random.Next(100, 1000);
+Console.WriteLine ( "Случайное трёхзначное число = "+randomNumber);
+
+int firstnumber = randomNumber / 100; // Получаем первую цифру
+
+int secondnumber = randomNumber % 10; // Получаем последнюю
+
+int thirdnumber = firstnumber * 10; // Превращем первое число в двухзначное
+
+int result =  thirdnumber + secondnumber; // Складываем двухзначное и последнее
+
+Console.WriteLine(result);
